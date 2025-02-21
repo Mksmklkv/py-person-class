1c4523dd8df54ac7d6c74f575360f06fe5cdae11
Maksym Kulakov <maksym.kulakov@ica.de>
2025-02-21 23:05:27 +0100

    Task updated

--- app/main.py
+++ app/main.py
@@ -8,15 +8,17 @@ class Person:
 
 
 def create_person_list(people: list) -> list[Person]:
-    person_instances = [Person(person["name"], person["age"])
-                        for person in people]
 
-    for person in people:
-        person_instance = Person.people[person["name"]]
+    if [len(people) != 0]:
+        person_instances = [Person(person["name"], person["age"])
+                            for person in people]
 
-        if "wife" in person and person["wife"] is not None:
-            person_instance.wife = Person.people[person["wife"]]
-        elif "husband" in person and person["husband"] is not None:
-            person_instance.husband = Person.people[person["husband"]]
+        for person in people:
+            person_instance = Person.people[person["name"]]
 
-    return person_instances
+            if "wife" in person and person["wife"] is not None:
+                person_instance.wife = Person.people[person["wife"]]
+            elif "husband" in person and person["husband"] is not None:
+                person_instance.husband = Person.people[person["husband"]]
+
+        return person_instances
